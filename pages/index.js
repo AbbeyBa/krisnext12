5b13cd9c953b83c6a851df64836dbc28ab3e7bc3
AbbeyBa <abbey@krisullis.com>
2022-10-15 03:13:02 +0530

    next index with image

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,7 @@ const HomePage = () => {
       </div>
       <div className="w-full md:w-1/2">
         <div className="w-full h-auto overflow-hidden rounded-md shadow-xl sm:rounded-xl" data-rounded="rounded-xl" data-rounded-max="rounded-full">
-            <img src="https://cdn.devdojo.com/images/november2020/hero-image.jpeg" className=""></img>
+            <image src="https://cdn.devdojo.com/images/november2020/hero-image.jpeg" className=""></image>
           </div>
       </div>
     </div>
