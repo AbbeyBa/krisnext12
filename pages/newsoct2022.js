83840433ee8177e501f5fb5f6e0085485f42bed6
AbbeyBa <abbey@krisullis.com>
2022-10-29 18:14:32 +0530

    newsletter derek update

--- pages/newsoct2022.js
+++ pages/newsoct2022.js
@@ -97,6 +97,201 @@ And for the minted NFT, visit here
         </p>
 </div>
 </div>
+
+<div className="flex flex-col max-w-6xl mx-auto">
+  <div className=""/>
+  <h2 className="text-xl mt-4 flex items-center font-bold tracking-tighter text-black sm:text-xl relative">
+            <span>Monthly Feature</span>
+        </h2>
+
+    <div className="grid grid-cols-6 gap-5 mt-0 md:grid-cols-12">
+        <p className="col-span-6 font-normal text-gray-700 lg:leading-8 xl:leading-8 md:text-xl text-sm sm:text-xl tracking-tight">
+        Let’s meet Derek Watson, founder of N2 Technology, which connects start-ups with the right investors. 
+                     
+              </p>  
+
+        <p className="col-span-6 font-normal text-gray-700 lg:leading-8 xl:leading-9 md:text-xl text-sm sm:text-xl tracking-tight">
+        <span className="text-sm text-gray-600"><span className="font-bold text-gray-900 mr-2">Duro: </span> Hello Derek, would you kindly introduce yourself?</span>
+ 
+        </p>
+
+        <p className="col-span-6 font-normal text-gray-700 lg:leading-8 xl:leading-9 md:text-xl text-sm sm:text-xl tracking-tight">
+        <span className="text-sm text-gray-600"><span className="font-bold text-gray-900 mr-2">Derek: </span> You want me to introduce myself, argh?</span>
+ 
+        </p>
+
+        <p className="col-span-6 font-normal text-gray-700 lg:leading-8 xl:leading-9 md:text-xl text-sm sm:text-xl tracking-tight">
+        <span className="text-sm text-gray-600"><span className="font-bold text-gray-900 mr-2">Duro: </span> Yeah, I do 😄</span>
+ 
+ 
+        </p>
+
+        <p className="col-span-6 font-normal text-gray-700 lg:leading-8 xl:leading-9 md:text-xl text-sm sm:text-xl tracking-tight">
+        <img className="mt-4 rounded-lg sm:max-h-72 max-w-xs" src="https://ngpzwhzdavpdovrvzium.supabase.co/storage/v1/object/public/krisdemoassets/image3_derek_oct_issue_2022.jpg"></img>
+        <span className="text-sm text-gray-600"><span className="font-bold text-gray-900 mr-2">Derek: </span> I am Derek Watson, I have a background in banking, financial markets and subsequently started a hedge fund which I managed for about 10 years and from this I got very interested in technology and the start-up economy, and this is what I do today. I meet many founders, and through a digital platform introduce them to potential investors and assist them along their journey. Which involves mentoring, market strategy, investor relationship. All those boring but necessary jobs at the beginning of a start-up, but I love it.</span>
+ 
+        </p>
+
+        <p className="col-span-6 font-normal text-gray-700 lg:leading-8 xl:leading-9 md:text-xl text-sm sm:text-xl tracking-tight">
+        <span className="text-sm text-gray-600"><span className="font-bold text-gray-900 mr-2">Duro: </span> I believe what you do is special, having to enjoy what you do, which we do not always have.</span>
+ 
+        </p>
+
+        <p className="col-span-6 font-normal text-gray-700 lg:leading-8 xl:leading-9 md:text-xl text-sm sm:text-xl tracking-tight">
+        <span className="text-sm text-gray-600"><span className="font-bold text-gray-900 mr-2">Derek: </span> 😄 It isn't always enjoyable and there is a lot of normality in there. But there is always this five-minute window whilst speaking with someone and they open your mind to a different way of thinking. This excites me, thinking about the future, how it will look in five to ten years and how we can make it better, not through the angle of money, which is important but from an angle of humanity, people who want to build things that benefit more people. And this is where my passion really stems from, these moments.</span>
+ 
+        </p>
+
+        <p className="col-span-6 font-normal text-gray-700 lg:leading-8 xl:leading-9 md:text-xl text-sm sm:text-xl tracking-tight">
+        <span className="text-sm text-gray-600"><span className="font-bold text-gray-900 mr-2">Duro: </span> Let’s talk about your entrepreneurial journey, the why and how.</span>
+ 
+        </p>
+
+        <p className="col-span-6 font-normal text-gray-700 lg:leading-8 xl:leading-9 md:text-xl text-sm sm:text-xl tracking-tight">
+        <span className="text-sm text-gray-600"><span className="font-bold text-gray-900 mr-2">Derek: </span> I have always been entrepreneurial from a young age. When I was a teenager, I wanted to earn some money; with a friend we started a car washing business with just my father’s bucket, sponge, and foaming liquid. We were at the local car wash every weekend and we had competitors, this led us to being innovative and learning to solve our problems as they arose. These little things formed me, especially getting into the corporate world, which is very entrepreneurial.</span>
+ 
+ 
+        </p>
+
+        <p className="col-span-6 font-normal text-gray-700 lg:leading-8 xl:leading-9 md:text-xl text-sm sm:text-xl tracking-tight">
+        <span className="text-sm text-gray-600">What launched my journey was when I became redundant due to new banking regulations at the time, leading to the start of a hedge fund. I knew my expertise and what I could be good at to make the business work, but I acknowledged the other aspects that I was not an expert in. I had to speak to lawyers, understand certain requirements and what it entailed for my business. I had to understand the marketing aspects of business, especially for start-ups. So, there isn’t a linear direction of what has to be done in entrepreneurship and this is the fascination in it. But you keep working until you find what works for you, then throw everything at what works with improvements along the way. If you do this correctly, notwithstanding the hurdles along the way, it should be fun, making things happen - this is huge.</span>
+ 
+        </p>
+
+        <p className="col-span-6 font-normal text-gray-700 lg:leading-8 xl:leading-9 md:text-xl text-sm sm:text-xl tracking-tight">
+        <span className="text-sm text-gray-600"><span className="font-bold text-gray-900 mr-2">Duro: </span> Does this mean I should not do what does not excite me?</span>
+ 
+ 
+        </p>
+
+        <p className="col-span-6 font-normal text-gray-700 lg:leading-8 xl:leading-9 md:text-xl text-sm sm:text-xl tracking-tight">
+        <span className="text-sm text-gray-600"><span className="font-bold text-gray-900 mr-2">Derek: </span> Yup, do not do it. It is very simple, because your motivation would be wrong and when things go wrong, you would give up. When you start, have the long-term vision in mind, but have the small success achievements measured by what you believe you can achieve at the same time. So, if you are not super passionate about it, do not understand a lot about your vertical or main expertise, your day would be super long and hard.</span>
+ 
+        </p>
+
+        <p className="col-span-6 font-normal text-gray-700 lg:leading-8 xl:leading-9 md:text-xl text-sm sm:text-xl tracking-tight">
+        <span className="text-sm text-gray-600"><span className="font-bold text-gray-900 mr-2">Duro: </span> You are an expert in investment, how do you decide which business is profitable to invest in? Do you have certain criteria?</span>
+ 
+ 
+        </p>
+
+
+        <p className="col-span-6 font-normal text-gray-700 lg:leading-8 xl:leading-9 md:text-xl text-sm sm:text-xl tracking-tight">
+        <span className="text-sm text-gray-600"><span className="font-bold text-gray-900 mr-2">Derek: </span> The main problem we identified is that the majority of start-ups do not have access to investors. It is difficult for these start-ups to connect with the right investors because we have an inverted market system. Normally a start-up sells its vision to investors through equity, who invest on the basis that the company would make money for its investors - this is the product. While in the physical marketplace, you see what you want to purchase before you buy it, for example a brown pair of shoes. There is a clearly defined route to buy the product. But in the investor market, an investor wants to see everything from the start, which makes it completely inverted to what it should be. This is what we try to solve.</span>
+ 
+        </p>
+
+        <p className="col-span-6 font-normal text-gray-700 lg:leading-8 xl:leading-9 md:text-xl text-sm sm:text-xl tracking-tight">
+        <span className="text-sm text-gray-600"> Investors have criteria based on how they see the future, on what technology they want to build on, on what type of entrepreneurs they want to invest into. From this, we match an investor to a start-up, getting rid of biases in the systems and unnecessary difficulties.</span>
+ 
+ 
+        </p>
+
+        <p className="col-span-6 font-normal text-gray-700 lg:leading-8 xl:leading-9 md:text-xl text-sm sm:text-xl tracking-tight">
+        <span className="text-sm text-gray-600"> I am a believer in having expert founders who are living the problems they are addressing. I prefer to invest in a person than in a product. That is why we want to drive these main founders, who are on the ground, who understand the issues and solutions, connect them with investors, and open access to funding. Yes, you can have many objective criteria for choosing a start-up to invest in, but the majority of what determines the investor’s choice is completely subjective - when there is a human connection, a level of trust.</span>
+ 
+ 
+        </p>
+
+        <p className="col-span-6 font-normal text-gray-700 lg:leading-8 xl:leading-9 md:text-xl text-sm sm:text-xl tracking-tight">
+        <span className="text-sm text-gray-600"><span className="font-bold text-gray-900 mr-2">Duro: </span>Objectively speaking, what do you think about the investment opportunities in the web 3, blockchain space and is it investment worthy? And if you believe it is, how can start-ups jump in on this?</span>
+ 
+        </p>
+
+        <p className="col-span-6 font-normal text-gray-700 lg:leading-8 xl:leading-9 md:text-xl text-sm sm:text-xl tracking-tight">
+        <span className="text-sm text-gray-600"><span className="font-bold text-gray-900 mr-2">Derek: </span> One hundred percent. I am a massive fan of what blockchain technology brings to the table - proof of ownership, smart contract - which can be utilised on absolutely anything. Looking at previous transitions from web 1 to web 2, taking about a 20-year period, we are just at the beginning of this journey, where we can use smart contracts and blockchain to speed up our transactions. But the problem is with the incumbents that make a lot of money out of it being the old system. So, they are trying to evolve, while still maintaining leverage in the market and retaining ownership in the business. But with every new technology, some businesses would lose a lot of revenue and market leverage. On the other hand, like with every new technology, we have people taking advantage of other people; and the hype does give blockchain technology a bad reputation.</span>
+ 
+ 
+        </p>
+
+        <p className="col-span-6 font-normal text-gray-700 lg:leading-8 xl:leading-9 md:text-xl text-sm sm:text-xl tracking-tight">
+        <span className="text-sm text-gray-600"> Nonetheless, with the underlying technology when utilised correctly, can do amazing things and this will happen. But with the hype, it becomes difficult to understand what is and what is not. But there is immense value for blockchain technology and web 3 once understood correctly. And there would be struggles along the way because of the incumbents who currently hold power, want to retain it but unfortunately may not be relevant with this new technology.</span>
+ 
+ 
+        </p>
+
+        <p className="col-span-6 font-normal text-gray-700 lg:leading-8 xl:leading-9 md:text-xl text-sm sm:text-xl tracking-tight">
+        <span className="text-sm text-gray-600"><span className="font-bold text-gray-900 mr-2">Duro: </span> So, this would be a game changer both within the technological space and for humanity.</span>
+ 
+        </p>
+
+        <p className="col-span-6 font-normal text-gray-700 lg:leading-8 xl:leading-9 md:text-xl text-sm sm:text-xl tracking-tight">
+        <span className="text-sm text-gray-600"><span className="font-bold text-gray-900 mr-2">Derek: </span> A hundred percent. I have no doubt that all filings, all monies, all identities would be on the blockchain. A digitalised economy, where all assets would gradually move unto the blockchain. I am a hundred percent certain that this will happen.</span>
+ 
+ 
+        </p>
+
+        <p className="col-span-6 font-normal text-gray-700 lg:leading-8 xl:leading-9 md:text-xl text-sm sm:text-xl tracking-tight">
+        <span className="text-sm text-gray-600"> We may have started the web 3 journey wrongly, because it was all about gaming and NFTs, which is good and was a start but along the way we lost the value proposition for humanity and what it can achieve. Therefore, creators and curators of information have to start taking ownership of what they produce, as opposed to what is existing on social media. A paradigm shift is underway, with originality, creating great content, having ownership, and being rewarded for it, web 3 can enhance this journey for us.</span>
+ 
+ 
+        </p>
+
+        <p className="col-span-6 font-normal text-gray-700 lg:leading-8 xl:leading-9 md:text-xl text-sm sm:text-xl tracking-tight">
+        <span className="text-sm text-gray-600"><span className="font-bold text-gray-900 mr-2">Duro: </span> Thank you so much Derek, what are your last words for us?</span>
+ 
+        </p>
+
+        <p className="col-span-6 font-normal text-gray-700 lg:leading-8 xl:leading-9 md:text-xl text-sm sm:text-xl tracking-tight">
+        <span className="text-sm text-gray-600"><span className="font-bold text-gray-900 mr-2">Derek: </span> If we use this technology in the right way and drive it in the right direction, we can reach a better, a more utopian world. But we have to embrace it and use it for the right things.</span>
+ 
+ 
+        </p>
+
+        <p className="col-span-6 font-normal text-gray-700 lg:leading-8 xl:leading-9 md:text-xl text-sm sm:text-xl tracking-tight">
+        <span className="text-sm text-gray-600"> It is a big journey for all of us, especially with all the noise around us, with everything around us becoming dollarised, we have to understand ourselves. Success isn’t about the dollars in the bank, sometimes success is about the simple things such as waking up in the morning with a smile on your face. These are achievements, therefore appreciate them, appreciate your time, appreciate everything you do, set realistic goals that are not just measured by money lest you get frustrated. Be yourself, embrace technology for the right reasons and remember you are amazing.</span>
+ 
+ 
+        </p>
+
+        <p className="col-span-6 font-normal text-gray-700 lg:leading-8 xl:leading-9 md:text-xl text-sm sm:text-xl tracking-tight">
+        <span className="text-sm text-gray-600"><span className="font-bold text-gray-900 mr-2">Reaching out</span></span>
+ 
+        </p>
+        
+        <p className="col-span-6 font-normal text-gray-700 lg:leading-8 xl:leading-9 md:text-xl text-sm sm:text-xl tracking-tight">
+        <span className="text-sm text-gray-600"> Interested in becoming a part of the Krisullis community through our onboarding workshop or you are an entrepreneur, educationalist, or sustainability advocate, do sign up here</span>
+        <a href="https://forms.gle/sDq5LSfsqixWqFbm8" className="inline-flex items-center  mx-2 pb-1 font-medium text-blue-500 border-b-2 border-gray-200 hover:border-blue-500 group">
+                    <span className="">Learn More</span>
+                    <svg className="w-5 h-5 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3"></path>
+                    </svg>
+                </a>
+ 
+        </p>
+
+        <p className="col-span-6 font-normal text-gray-700 lg:leading-8 xl:leading-9 md:text-xl text-sm sm:text-xl tracking-tight">
+        <span className="text-sm text-gray-600"><span className="font-bold text-gray-900 mr-2">Derek: </span> Yeah, I do 😄</span>
+ 
+ 
+        </p>
+
+
+                <p className="col-span-6 font-normal text-gray-700 lg:leading-8 xl:leading-9 md:text-xl text-sm sm:text-xl tracking-tight">
+                The goal is to empower these people to embrace their diversities and individualities and use it as their strength in the web 3 and metaverse world. 
+For more on the interview, kindly visit here 
+
+<a href="https://www.youtube.com/watch?v=x-W6S6BsXx8" className="inline-flex items-center  mx-2 pb-1 font-medium text-blue-500 border-b-2 border-gray-200 hover:border-blue-500 group">
+                    <span className="">Learn More</span>
+                    <svg className="w-5 h-5 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3"></path>
+                    </svg>
+                </a> 
+</p>
+
+<p className="col-span-6 font-normal text-gray-700 lg:leading-8 xl:leading-9 md:text-xl text-sm sm:text-xl tracking-tight">
+And for the minted NFT, visit here
+
+<a href="https://www.mintbase.io/contract/krisullismerch.mintbase1.near/token/42" className="inline-flex items-center  mx-2 pb-1 font-medium text-blue-500 border-b-2 border-gray-200 hover:border-blue-500 group">
+                    <span className="">Learn More</span>
+                    <svg className="w-5 h-5 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3"></path>
+                    </svg>
+                </a> 
+
+        </p>
+</div>
+</div>
 </div>
       
   
