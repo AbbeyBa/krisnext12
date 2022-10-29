4731abd99992c3f777fb4e078fc0fdb519b92d39
AbbeyBa <abbey@krisullis.com>
2022-10-29 17:18:20 +0530

    newsletter giselle update, lets see how it looks

--- pages/newsoct2022.js
+++ pages/newsoct2022.js
@@ -10,35 +10,97 @@ function newsoct2022() {
       <title>Krisullis - You Belong Here</title>
     </Head>
     <Header />
-    <div className="max-w-7xl px-5 py-20 flex flex-col space-y-5 w-full h-full items-center justify-center mx-auto">
-        <div className="flex h-full w-full rounded-md">
-
-        <div class="max-w-3xl px-5 py-20 flex flex-col w-full h-full items-start justify-center mx-auto relative">
-        <h2 class="text-4xl flex items-center font-bold tracking-tighter text-black sm:text-5xl relative">
+    <div className="w-full px-8 pt-5 pb-16 bg-white xl:px-0">
+  <div className="flex flex-col max-w-6xl mx-auto">
+  <h2 className="text-3xl flex items-center font-bold tracking-tighter text-black sm:text-3xl relative">
             <span>An Intro</span>
         </h2>
-        <p class="mt-4 text-xl sm:text-xl tracking-tight text-gray-700">Krisullis is a transformational Web 3 organisation that simply seeks to connect modern people with technology that benefits you. Enables you to create and own your content; learn solely about Web 3 and its processes; and channel your resources through the web 3 model, making you create a version of modified and a seamless reality for yourself and others. Ready? Let’s jump right in.</p>
-    </div>
 
-
-        </div>
-        <div className="flex space-x-5 w-full h-full">
-            <div className="flex h-full w-full rounded-md">
-            <div class="max-w-3xl px-5 py-20 flex flex-col w-full h-full items-start justify-center mx-auto relative">
-        <h2 class="text-2xl flex items-center font-bold tracking-tighter text-black sm:text-2xl relative">
+    <div className="">
+        <p className="col-span-6 font-normal text-gray-700 md:text-xl text-sm sm:text-xl tracking-tight">
+        Krisullis is a transformational Web 3 organisation that simply seeks to connect modern people with technology that benefits you. Enables you to create and own your content; learn solely about Web 3 and its processes; and channel your resources through the web 3 model, making you create a version of modified and a seamless reality for yourself and others. Ready? Let’s jump right in.</p>
+</div>
+</div>
+        
+<div className="flex flex-col max-w-6xl mx-auto">
+  <div className=""/>
+  <h2 className="text-xl mt-4 flex items-center font-bold tracking-tighter text-black sm:text-xl relative">
             <span>Onboarding Changemakers</span>
         </h2>
-        <p class="mt-4 text-xl sm:text-xl tracking-tight text-gray-700">This month, we got started with our fourth cohort of changemakers. These are youths from around the world, who go through a six-week training that educates them about web 3, blockchain and crypto. 
 
-The cohort was launched on the 22nd of October, set to end early December. This cohort comprises members from the Youth Sustainable Development Network, Grassroots to Global Sustainable Development Network, Sustainable Green Development Initiative, and Eco Diversified International to mention but a few. 
+    <div className="grid grid-cols-6 gap-5 mt-0 md:grid-cols-12">
+        <p className="col-span-6 font-normal text-gray-700 lg:leading-8 xl:leading-8 md:text-xl text-sm sm:text-xl tracking-tight">
+        This month, we got started with our fourth cohort of changemakers. These are youths from around the world, who go through a six-week training that educates them about web 3, blockchain and crypto. The cohort was launched on the 22nd of October, set to end early December. This cohort comprises members from the Youth Sustainable Development Network, Grassroots to Global Sustainable Development Network, Sustainable Green Development Initiative, and Eco Diversified International to mention but a few. 
+        
+
+        
+                <img className="mt-4 rounded-lg sm:max-h-72 max-w-sm" src="https://ngpzwhzdavpdovrvzium.supabase.co/storage/v1/object/public/krisdemoassets/image_un_sdg_oct_issue_2022.jpg"></img>
+              </p>  
+
+        <p className="col-span-6 font-normal text-gray-700 lg:leading-8 xl:leading-9 md:text-xl text-sm sm:text-xl tracking-tight">
+        Krisullis is intentional about educating youths from across various sectors of society and this cohort focuses on changemakers in sustainable development, a crucial and evolving aspect of society.
+        </p>
+</div>
+</div>
+<div className="flex flex-col max-w-6xl mx-auto">
+  <div className=""/>
+  <h2 className="text-xl mt-4 flex items-center font-bold tracking-tighter text-black sm:text-xl relative">
+            <span>Krisullis Expert Series: Giselle Mota</span>
+        </h2>
+
+    <div className="grid grid-cols-6 gap-5 mt-0 md:grid-cols-12">
+        <p className="col-span-6 font-normal text-gray-700 lg:leading-8 xl:leading-8 md:text-xl text-sm sm:text-xl tracking-tight">
+        A Future of Work principal consultant at ADP, with a passion for maths, Artificial Intelligence, web 3 technologies and diversity, equity, and inclusion; areas and trends necessary to the future of work. She enjoys serving underserved communities in relation to diversity, equity, and inclusion.
+
+In the conversation we had with her, she underscored the multidimensional and nuanced nature of human beings. Highlighting there needs to be an embrace of diversity and a value of it, because of the strength and uniqueness it provides. She emphasised that conversations and solutions need to be proffered to current inequities, because emerging technology and the Covid-19 pandemic has made these loopholes more glaring, and racial and social injustice are also at its height.
+ 
+        
+
+        
+                <img className="mt-4 rounded-lg sm:max-h-72 max-w-sm" src="https://ngpzwhzdavpdovrvzium.supabase.co/storage/v1/object/public/krisdemoassets/image2_giselle_oct_issue_2022.jpg"></img>
+              </p>  
+
+        <p className="col-span-6 font-normal text-gray-700 lg:leading-8 xl:leading-9 md:text-xl text-sm sm:text-xl tracking-tight">
+        As a response to many of these disparities, Giselle created an NFT project called Nfty Collective, which ensures there is a safe and inclusive space in web 3 and metaverse for people with disabilities and various diversities. With a belief that nobody should be left behind at the pace in which technology is accelerating, she created NFT collections that show various disabilities of people that do not otherwise exist in the current web 3 and metaverse space. Such as muscular dystrophy, tetra-amelia, wheelchair users, to mention but a few. The collective can be accessed here
+        
+        <a href="https://imnfty.com/" className="inline-flex items-center  mx-2 pb-1 font-medium text-blue-500 border-b-2 border-gray-200 hover:border-blue-500 group">
+                    <span className="">Learn More</span>
+                    <svg className="w-5 h-5 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3"></path>
+                    </svg>
+                </a> 
+        
+        
+        </p>
+                <p className="col-span-6 font-normal text-gray-700 lg:leading-8 xl:leading-9 md:text-xl text-sm sm:text-xl tracking-tight">
+                The goal is to empower these people to embrace their diversities and individualities and use it as their strength in the web 3 and metaverse world. 
+For more on the interview, kindly visit here 
+
+<a href="https://www.youtube.com/watch?v=x-W6S6BsXx8" className="inline-flex items-center  mx-2 pb-1 font-medium text-blue-500 border-b-2 border-gray-200 hover:border-blue-500 group">
+                    <span className="">Learn More</span>
+                    <svg className="w-5 h-5 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3"></path>
+                    </svg>
+                </a> 
 </p>
-    </div>
 
+<p className="col-span-6 font-normal text-gray-700 lg:leading-8 xl:leading-9 md:text-xl text-sm sm:text-xl tracking-tight">
+And for the minted NFT, visit here
 
-            </div>
-            <div className="flex h-full w-full bg-gray-300 rounded-md"></div>
-        </div>
-    </div>
+<a href="https://www.mintbase.io/contract/krisullismerch.mintbase1.near/token/42" className="inline-flex items-center  mx-2 pb-1 font-medium text-blue-500 border-b-2 border-gray-200 hover:border-blue-500 group">
+                    <span className="">Learn More</span>
+                    <svg className="w-5 h-5 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3"></path>
+                    </svg>
+                </a> 
+
+        </p>
+</div>
+</div>
+</div>
+      
+  
+
 
 </>
   )
