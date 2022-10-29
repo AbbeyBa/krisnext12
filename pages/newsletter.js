3d6a145752a413cae78d9cec2ed54708d13275aa
AbbeyBa <abbey@krisullis.com>
2022-10-29 10:30:34 +0530

    newsletter size check

--- pages/newsletter.js
+++ pages/newsletter.js
@@ -17,14 +17,14 @@ function newsletter() {
 
         <div className="flex grid h-auto grid-cols-12 gap-10 pb-10 mt-8 sm:mt-16">
             <div className="relative flex flex-col items-start justify-end h-full col-span-12 overflow-hidden rounded-xl group md:col-span-6 xl:col-span-4">
-                <a href="#_" className="block w-full transition duration-300 ease-in-out transform bg-center bg-cover h-auto hover:scale-110">
-                <img className="object-cover h-full rounded-lg sm:max-h-full" src="https://ngpzwhzdavpdovrvzium.supabase.co/storage/v1/object/public/krisdemoassets/KRISULLIS_SEPT_issue_2022.jpg"></img>
+                <a href="#_" className="block">
+                <img className="object-cover h-full rounded-lg sm:max-h-full xs:max-h-full" src="https://ngpzwhzdavpdovrvzium.supabase.co/storage/v1/object/public/krisdemoassets/KRISULLIS_SEPT_issue_2022.jpg"></img>
                 </a>
                 
         </div>
         <div className="relative flex flex-col items-start justify-end h-full col-span-12 overflow-hidden rounded-xl group md:col-span-6 xl:col-span-4">
-                <a href="/newsoct2022" className="block w-full transition duration-300 ease-in-out transform bg-center bg-cover h-auto hover:scale-110">
-                <img className="object-cover h-full rounded-lg sm:max-h-full" src="https://ngpzwhzdavpdovrvzium.supabase.co/storage/v1/object/public/krisdemoassets/Coverpage_oct_issue_2022.png"></img>
+                <a href="/newsoct2022" className="block">
+                <img className="object-cover h-full rounded-lg sm:max-h-full xs:max-h-full" src="https://ngpzwhzdavpdovrvzium.supabase.co/storage/v1/object/public/krisdemoassets/Coverpage_oct_issue_2022.png"></img>
                 </a>
                 
         </div>
