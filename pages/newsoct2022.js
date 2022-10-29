54e1e9b213bcf11a74f451829af6885482e9d163
AbbeyBa <abbey@krisullis.com>
2022-10-29 11:23:27 +0530

    newsletter content start test

--- pages/newsoct2022.js
+++ pages/newsoct2022.js
@@ -11,9 +11,31 @@ function newsoct2022() {
     </Head>
     <Header />
     <div className="max-w-7xl px-5 py-20 flex flex-col space-y-5 w-full h-full items-center justify-center mx-auto">
-        <div className="flex h-full w-full bg-gray-300 rounded-md"></div>
+        <div className="flex h-full w-full rounded-md">
+
+        <div class="max-w-3xl px-5 py-20 flex flex-col w-full h-full items-start justify-center mx-auto relative">
+        <h2 class="text-4xl flex items-center font-bold tracking-tighter text-black sm:text-5xl relative">
+            <span>An Intro</span>
+        </h2>
+        <p class="mt-4 text-xl sm:text-xl tracking-tight text-gray-700">Krisullis is a transformational Web 3 organisation that simply seeks to connect modern people with technology that benefits you. Enables you to create and own your content; learn solely about Web 3 and its processes; and channel your resources through the web 3 model, making you create a version of modified and a seamless reality for yourself and others. Ready? Let’s jump right in.</p>
+    </div>
+
+
+        </div>
         <div className="flex space-x-5 w-full h-full">
-            <div className="flex h-full w-full bg-gray-300 rounded-md"></div>
+            <div className="flex h-full w-full rounded-md">
+            <div class="max-w-3xl px-5 py-20 flex flex-col w-full h-full items-start justify-center mx-auto relative">
+        <h2 class="text-2xl flex items-center font-bold tracking-tighter text-black sm:text-2xl relative">
+            <span>Onboarding Changemakers</span>
+        </h2>
+        <p class="mt-4 text-xl sm:text-xl tracking-tight text-gray-700">This month, we got started with our fourth cohort of changemakers. These are youths from around the world, who go through a six-week training that educates them about web 3, blockchain and crypto. 
+
+The cohort was launched on the 22nd of October, set to end early December. This cohort comprises members from the Youth Sustainable Development Network, Grassroots to Global Sustainable Development Network, Sustainable Green Development Initiative, and Eco Diversified International to mention but a few. 
+</p>
+    </div>
+
+
+            </div>
             <div className="flex h-full w-full bg-gray-300 rounded-md"></div>
         </div>
     </div>
